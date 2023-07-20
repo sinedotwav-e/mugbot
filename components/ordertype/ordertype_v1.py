f7d806c71d68358380d69d618c18aaffd2c85101
sinedotwav <sinedotwav-e@users.noreply.github.com>
2023-07-21 10:29:08 +1200

    Created & modified multiple files
    Modified welcome_v1
    
    Created ordertype_v1

---
+++ components/ordertype/ordertype_v1.py
@@ -0,0 +1,9 @@
+# Allows user to order for either delivery or click-and-collect
+import time
+
+print("Would you like to order for click-and-collect or delivery?")
+print("")
+print("Enter 1 for Click-and-collect")
+print("Enter 2 for Delivery ($9.00 surcharge)")
+
+ordtype = input(int("Enter here > "))
--- components/welcome/welcome_v1.py
+++ components/welcome/welcome_v1.py
@@ -1,14 +1,18 @@
+# Welcomes the user, and bot introduces itself with a randomly generated name
 import random
 from random import randint
 import time
 
-names = ["Doug", "Douglas", "Dougington", "Dougie", "Duggie", "Doogie", "Dougalasa", "Dùbhghlas", "Thrangott the Devourer", "Gab", "Doug Doug"]
+names = ["Doug", "Douglas", "Dougington", "Dougie", "Duggie", "Doogie", "Dougalasa", 
+"Dùbhghlas", "Thrangott the Devourer", "Gab", "Doug Doug"]
 
 def welcome():
     num = randint(1,10)
     name = (names[num])
+
     print("Welcome to MUGSHOT!")
-    time.sleep(0.5)
+    time.sleep(1.5)
     print("My name is", name, "and I will help you order a brand new mug!")
+    time.sleep(1.8)
 
 welcome()
