e2d69dbb7a9c1f130183857842ac13031c260511
sinedotwav-e <55688664+sinedotwav-e@users.noreply.github.com>
2023-07-28 06:44:32 +1200

    Created multiple files
    Created ordering_v3
    
    Created ordering_vf

---
+++ components/07 ordering/ordering_v3.py
@@ -0,0 +1,70 @@
+# Allows users to select what they want from the menu provided
+
+# Names of mugs
+mugnames = ['Plain white mug', 'Plain black mug', 'Plain brown mug', 'Plain green mug', # 1-4 Single tone mugs
+            'Striped white & red mug', 'Striped black & yellow mug', 'Gradient magenta & blue mug', 'Gradient black and white mug', # 5-8 Patterned mugs
+            'Two-tone green & white mug', 'Two-tone brown & white mug', 'Two-tone blue & purple mug', 'Two-tone black & white mug', # 9-12 Two-toned mugs
+            'White skull decal mug', 'Green leaf decal mug', 'Blue sun decal mug', 'Purple gem decal mug', # 13-16 Mugs with a decal
+            'Glow-in-the-dark mug', 'Glitter mug', 'Colour changing mug', 'Glass mug', # 17-20 Unique feature mugs
+            'Skull shaped mug', 'Wooden tankard', 'Wallace mug (from Wallace & Gromit)', 'Golden, diamond encrusted bowl'] # 21-24 Unique shape mugs
+
+# Prices of mugs
+mugprices = [12.50, 12.50, 12.50, 12.50, 14.20, 14.20, 14.20, 14.20, 14.20, 14.20, 14.20, 14.20, # 1-4 Single tone mugs, 5-8 Patterned mugs, 9-12 Two-toned mugs
+             16.80, 16.80, 16.80, 16.80, 20.00, 20.00, 20.00, 20.00, 24.00, 24.00, 30.00, 50.00] # 13-16 Mugs with a decal, 17-20 Unique feature mugs, 21-24 Unique shap
+
+mugtotal = 24 # Total number of mugs
+
+def mugmenu():
+    print("Select your mugs from the list below!\n-----------------------------------------") # Menu title
+
+    for count in range (mugtotal): # For loop printing the index number, price and item name
+        print("{} ${:.2f} {}"  .format(count+1, float(mugprices[count]), mugnames[count])) # Formatted menu
+    print()
+
+mugmenu()
+
+
+# List to stored ordered mugs
+orderlist = []
+# List to store ordered mugss prices
+ordercost = []
+
+# Ask for number of mugs required
+mugsnum = 0
+
+while True:
+    try:
+        mugsnum = int(input("How many mugs would you like to order? > "))
+        if mugsnum >= 1 and mugsnum <= 20:
+            break
+        else:
+            print("\nYour order must be between 1 and 20")
+
+    except ValueError:
+        print ("\nThat is not a valid number")
+        print ("Please enter a number between 1 and 20")
+
+print("\nYou are ordering", mugsnum, "mugs.\n")
+
+for item in range(mugsnum):
+    while mugsnum > 0:
+        while True:
+            try:
+                mugordered = int(input("By entering the index number on the left, order your mugs > "))
+                if mugordered >= 1 and mugordered <= 24:
+                    break
+                else: 
+                    print("Your number must be between 1 and 24")
+                    
+            except ValueError:
+                    print ("That is not a valid number")
+                    print ("Please enter number enter between 1 and 24 ")
+                    
+        orderlist.append(mugnames[mugordered])
+        ordercost.append(mugprices[mugordered])
+        print("You have ordered a ${:.2f} {}" .format(mugprices[mugordered], mugnames[mugordered]))
+        mugsnum = mugsnum - 1
+        print("\n", mugsnum, "mugs remaining")
+
+print(orderlist)
+print(ordercost)
