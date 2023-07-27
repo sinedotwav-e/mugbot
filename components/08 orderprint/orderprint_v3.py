3e1807e168219f6f95057241b771e6cbdec61b51
sinedotwav-e <55688664+sinedotwav-e@users.noreply.github.com>
2023-07-28 07:36:01 +1200

    Modified orderprint_v3
    Modified orderprint_v3

--- components/08 orderprint/orderprint_v3.py
+++ components/08 orderprint/orderprint_v3.py
@@ -3,14 +3,19 @@
 # Dictionary of stored user details
 userdetails = {'name':'Earf', 'phone':'123456789', 'house':'24', 'street':'Manapouri Pl', 'suburb':'Pakuranga'} # Placeholder details
 # List to stored ordered mugs
-orderlist = ['Plain white mug', 'Plain black mug'] # Placeholder items
+orderlist = ['Plain white mug', 'Glow-in-the-dark mug'] # Placeholder items
 # List to store ordered mugss prices
-ordercost = [12.60, 12.60] # Placeholder prices
+ordercost = [12.60, 20.00] # Placeholder prices
+
+totalcost = sum(ordercost)
 
 print("\nName: {}\nPhone Number: {}\nHouse Number: {}\nStreet Name: {}\nSuburb: {}\n"
       .format(userdetails['name'], userdetails['phone'], userdetails['house'], userdetails['street'], userdetails['suburb']))
 
 count = 0
 for item in orderlist:
-    print("Ordered: {}\nCost: ${:.2f}" .format(item, ordercost[count]))
+    print("Ordered: {} | Cost: ${:.2f}" .format(item, ordercost[count]))
     count = count + 1
+
+print("\nTotal order cost")
+print(f"${totalcost:.2f}")
